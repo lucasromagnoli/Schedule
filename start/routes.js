71df75c8fd6bd5551e9c955f7192ea6512c3acfc
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-05-29 23:15:23 -0300

    Controller.ForgotPassword - Update

--- start/routes.js
+++ start/routes.js
@@ -5,4 +5,6 @@ const Route = use('Route')
 
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
+
 Route.post('forgotpasswords', 'ForgotPasswordController.store')
+Route.put('forgotpasswords', 'ForgotPasswordController.update')
