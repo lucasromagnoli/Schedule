4eadb145f5901dea0ed214d4471102083b85bd6d
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-06-09 20:15:59 -0300

    Sentry - Error tracking in production

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -2,7 +2,7 @@
 
 const Env = use('Env')
 const Youch = use('Youch')
-
+const sentry = use('Sentry')
 const BaseExceptionHandler = use('BaseExceptionHandler')
 
 /**
@@ -48,7 +48,7 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log('error :', error)
+    sentry.captureException(error)
   }
 }
 
